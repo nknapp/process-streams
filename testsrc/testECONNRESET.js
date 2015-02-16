0254e5ee7bf991e1b18d8e21b57da49fd24a3ee8
Nils Knappmeier <n.knappmeier@i-views.de>
2015-02-16 23:13:27 +0100

    Adapt testECONNRESET.js to video-testdata-loader

--- testsrc/testECONNRESET.js
+++ testsrc/testECONNRESET.js
@@ -2,37 +2,13 @@ var ProcessStreams = require("../src/process-streams.js");
 var fs = require("fs");
 var es = require("event-stream");
 var path = require("path");
-var panasonicTar = require.resolve("video-testdata/data/panasonic-lumix-dmc-zx3.tar");
 var ps = new ProcessStreams();
-var unpack = require('tar').Extract;
-var tmpDir = require("tempfile")(".tar");
-console.log(tmpDir);
-var mtsFile = path.join(tmpDir, "PRIVATE", "AVCHD", "BDMV", "STREAM", "00000.MTS");
 
-
-function unpackMTS(callback) {
-    fs.exists(tmpDir, function (err, exists) {
-        if (err) {
-            return callback(err);
-        }
-        if (exists) {
-            callback(null);
-        } else {
-            fs.createReadStream(panasonicTar)
-                .pipe(unpack({ path: tmpDir}))
-                .pipe(es.wait(callback));
-        }
-    });
-
-}
 
 module.exports.testECONNRESET = function (test) {
     test.expect(1);
-    unpackMTS(function(err) {
-        if (err) throw err;
-        var input = fs.createReadStream(mtsFile);
-        console.log(tmpDir,fs.existsSync(mtsFile));
-
+    require("video-testdata-loader")("panasonic-lumix-dmc-zx3.tar", "PRIVATE/AVCHD/BDMV/STREAM/00000.MTS", function (err, file) {
+        var input = fs.createReadStream(file);
         var spawn = ps.spawn("exiftool", ["-s3", "-MimeType", "-fast", "-"]);
         spawn.setEncoding("utf-8");
         input.pipe(spawn).on("input-closed", function (error) {
