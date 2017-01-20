8e890ef8f069c934827f003e148aa172645c7c9a
Nils Knappmeier <github@knappi.org>
2017-01-20 12:27:58 +0100

    chore(modernize)
    - Test case fixes
    - Use "thought" for documentation
    - Setup "travis"

--- testsrc/testECONNRESET.js
+++ testsrc/testECONNRESET.js
@@ -1,35 +1,48 @@
-var ProcessStreams = require("../src/process-streams.js");
-var fs = require("fs");
-var es = require("event-stream");
-var path = require("path");
-//require("trace");
-//require("clarify");
-//Error.stackTraceLimit = 50;
+var ProcessStreams = require('../src/process-streams.js')
+var fs = require('fs')
+var es = require('event-stream')
+require('trace-and-clarify-if-possible')
 
-var ps = new ProcessStreams();
-
+var ps = new ProcessStreams()
 
 module.exports.testECONNRESET = function (test) {
-    test.expect(1);
-    require("video-testdata-loader")("panasonic-lumix-dmc-zx3.tar", "PRIVATE/AVCHD/BDMV/STREAM/00000.MTS", function (err, file) {
-        var input = fs.createReadStream(file);
-        var spawn = ps.spawn("exiftool", ["-s3", "-MimeType", "-fast", "-"]);
-        spawn.setEncoding("utf-8");
-        input.pipe(spawn).on("input-closed", function (error) {
-            test.equal(error.code, "ECONNRESET");
-        }).pipe(es.wait(function () {
-            console.log(arguments);
-            test.done();
-        }));
-    });
-};
+  test.expect(2)
+  require('video-testdata-loader')('panasonic-lumix-dmc-zx3.tar', 'PRIVATE/AVCHD/BDMV/STREAM/00000.MTS', function (err, file) {
+    if (err) throw err
+    var input = fs.createReadStream(file)
+    var spawn = ps.spawn('exiftool', ['-s3', '-MimeType', '-fast', '-'])
+    spawn.setEncoding('utf-8')
+    input
+      .pipe(spawn)
+      .on('input-closed', function (error) {
+        test.equal(error.code, 'ECONNRESET')
+      })
+      .pipe(es.wait(function (err, output) {
+        if (err) {
+          throw err
+        }
+        // Delay test for a while to let the input-closed event happen
+        setTimeout(test.done, 50)
+        test.deepEqual(output, 'video/m2ts\n', 'Checking returned result')
+      }))
+  })
+}
 
 module.exports.testEPIPE = function (test) {
-    var spawn2 = ps.spawn("head", ["-2"]);
+  var spawn2 = ps.spawn('head', ['-2'])
 
-    spawn2.setEncoding("utf-8");
-    es.readArray(["1\n", "2\n", "2\n", "2\n"]).pipe(spawn2).pipe(es.wait(function () {
-        console.log(arguments);
-        test.done();
-    }));
-};
+  spawn2.setEncoding('utf-8')
+  es.readArray(['1\n', '2\n', '2\n', '2\n'])
+    .pipe(spawn2)
+    .on('input-closed', function (error) {
+      test.equal(error.code, 'EPIPE')
+    })
+    .pipe(es.wait(function (err, output) {
+      if (err) {
+        throw err
+      }
+      // Delay test for a while to let the input-closed event happen
+      setTimeout(test.done, 50)
+      test.deepEqual(output, '1\n2\n', 'Checking returned result')
+    }))
+}
