e60d7d20b18cbba849542054bb8a7b51b5c55dd1
Nils Knappmeier <npm@knappi.org>
2024-07-14 14:30:14 +0200

    chore: fix prepare script for node 0.10

--- scripts/on-prepare.js
+++ scripts/on-prepare.js
@@ -1,6 +1,6 @@
 const cp = require('child_process')
 
-cp.exec('git config core.hooksPath .git-hooks', { stdio: 'inherit' }, error => {
+cp.exec('git config core.hooksPath .git-hooks', { stdio: 'inherit' }, function (error) {
     if (error) {
         console.error(error)
         process.exit(1)
